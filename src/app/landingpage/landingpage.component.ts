91f4325a1746b3e9e3addaedd51cc7718765b16f
Ayanrinde <ayanrindegaius005@gmail.com>
2024-03-13 13:49:57 +0100

    in progress

--- src/app/landingpage/landingpage.component.ts
+++ src/app/landingpage/landingpage.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
-
+interface objectone{
+  age:number,
+  school:string,
+  address:string
+}
 @Component({
   selector: 'app-landingpage',
   standalone: true,
@@ -8,5 +12,33 @@ import { Component } from '@angular/core';
   styleUrl: './landingpage.component.css'
 })
 export class LandingpageComponent {
+// number, string,boolean,object,array,undefined
+// inference, union
 
+  public firstname:string|number|boolean=true
+  public age:number=67
+  public special:boolean=false
+  public x:undefined|number|string
+
+public obj:objectone={
+  age:56,
+  address:'Ogbomosho',
+  school:'SQI'
+}
+
+public arrayone:number[]=[1,3,5]
+
+
+  ngOnInit(){
+    console.log(this.obj);
+    
+    let firstname=90
+    let x
+    x=56
+
+  this.x=''
+    this.firstname='Dammy'
+    this.age=100
+    console.log(this.firstname);
+  }
 }
